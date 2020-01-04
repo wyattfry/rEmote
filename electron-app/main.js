bbf893dff642df2ccb69d5f6c86d6abe6760d072
Wyatt Fry <wyattfry@gmail.com>
2020-01-04 12:40:00 -0500

    r and w img directories in settings file

--- electron-app/main.js
+++ electron-app/main.js
@@ -1,11 +1,32 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const fs = require('fs');
 
-// dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] }).then(console.log)
+let settings = {};
+
+settings.settingsFileName = "settings.json";
+
+settings.imageDirectories = {
+  "backgrounds": null,
+  "characters": null,
+  "costumes": null,
+  "emotions": null,
+};
 
 app.on('ready', function () {
-  createMainWindow();
-  createDisplayWindow();
+  loadOrCreateSettings(settingsFileExisted => {
+    createMainWindow(()=>{
+      if (settingsFileExisted) {
+        // If any image directories saved in loaded settings file, load
+        // those images as buttons in control window.
+        Object.keys(settings.imageDirectories).forEach(layer => {
+          if (settings.imageDirectories[layer] != null) {
+            loadImages(layer);
+          }
+        });
+      }
+    });
+    createDisplayWindow();
+  });
 })
 
 app.on('window-all-closed', () => {
@@ -24,9 +45,31 @@ app.on('activate', () => {
   }
 })
 
+function loadOrCreateSettings(callback) {
+  fs.exists(settings.settingsFileName, (exists) => {
+    if (exists) {
+      fs.readFile(settings.settingsFileName, function(err, data){
+        if (err) {
+          console.error("Error reading settings.json");
+          // does it not exist? thus create?
+          return;
+        }
+        settings = JSON.parse(data);
+        callback(true);
+      });
+    } else {
+      callback(false);
+      // create settings file if it does not already exist
+      fs.writeFile(settings.settingsFileName, JSON.stringify(settings), ()=>{});
+    }
+  });
+}
+
+
 // Control Window
 let controlWindow;
-async function createMainWindow() {
+
+async function createMainWindow(callback) {
   controlWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -35,20 +78,22 @@ async function createMainWindow() {
     }
   });
   controlWindow.webContents.openDevTools();
-  await controlWindow.loadFile('controlWindow.html');
-  // page has loaded, ipcRenderer is ready to receive events
-  const layerNames = ["backgrounds", "characters", "costumes", "emotions"];
-  layerNames.forEach(l => loadImages(controlWindow, l));
+  controlWindow.loadFile('controlWindow.html').then(callback);
   controlWindow.on('closed', () => controlWindow = null);
 }
 
 // Listen for `showImage` event from mainWindow, emit a new one to the displayWindow
-ipcMain.on('showImage', function (e, layer, imagePath) {
-  displayWindow.webContents.send('showImage', layer, imagePath);
+ipcMain.on('showImage', function (e, layer, fileName) {
+  displayWindow.webContents.send('showImage', layer, settings.imageDirectories[layer], fileName);
+});
+
+ipcMain.on('setImageDirectory', function (e, layer) {
+  setImageDirectory(layer);
 });
 
 // Display Window
 let displayWindow;
+
 function createDisplayWindow() {
   displayWindow = new BrowserWindow({
     width: 640,
@@ -62,11 +107,27 @@ function createDisplayWindow() {
   displayWindow.on('closed', () => displayWindow = null);
 }
 
-function loadImages(window, layer) {
-  fs.readdir(layer, function (err, files) {
+function loadImages(layer) {
+  fs.readdir(settings.imageDirectories[layer], function (err, files) {
     if (err) {
       console.error('Could not read directory', layer);
     }
     controlWindow.webContents.send('loadImages', layer, files);
   });
 }
+
+function setImageDirectory(layer) {
+  dialog.showOpenDialog({
+    properties: ['openDirectory'],
+  }).then(result => {
+    if (result.canceled) {
+      return;
+    }
+    settings.imageDirectories[layer] = result.filePaths[0];
+    fs.writeFile(settings.settingsFileName, JSON.stringify(settings), ()=>{})
+    loadImages(layer);
+  });
+}
+
+// file:///C:/Users/Wyatt/Projects/rEmote/electron-app/backgrounds/background.png
+// file:///C:/Users/Wyatt/Projects/rEmote/electron-app/background.png
